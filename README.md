4a9857cbcb0b5f50e0464ecef0c84f312d85f921
marereis <marereis@gmail.com>
2023-11-13 09:05:01 -0300

    commit images- readme

--- README.md
+++
@@ -1,5 +0,0 @@
-<h1 align="center">
-    <img alt="Lotris - Gestao de RH" title="Lotris - Gestao de RH" src=".resources/assets/img/login-rh-lotris.png" />
-</h1>
- # php-RH-lotris - Lotris - Gestao de RH
-
---
+++ bootstrap/Config.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * SITE CONFIG
+ */
+define("SITE", [
+    "name" => "Lotris RH",
+    "desc" => "Lotris RH -  Sorfware de Gestao Recursos Humanos",
+    "domain" => "lotris",
+    "locale" => "pt_BR",
+    "root" => "http://localhost:81/rhlotris/",
+    "root1" => "http://localhost:8080/rhlotris/",
+    "root1" => "http://localhost:81/rhlotris/",
+]);
+
+/**
+ * DATABASE CONECT
+ */
+define("DATA", [
+    "driver" => "mysql",
+    "host" => "127.0.0.1",
+    "port" => "3306",
+    "dbname" => "bdrh",
+    "username" => "root",
+     "passwd" => "mare127601@aA",
+   // "passwd" => "",
+    "options" => [
+        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+        PDO::ATTR_CASE => PDO::CASE_NATURAL
+    ]
+]);
+
+/**
+ * PASSWORD
+ */
+define("CONF_PASSWD_ALGO", PASSWORD_DEFAULT);
+define("CONFI_PASSWD_OPTION", ["cost" => 10]);
+
+/**
+ * Time Zone
+ */
+define("TIME_ZONE", "America/Sao_Paulo");
+
+
+
